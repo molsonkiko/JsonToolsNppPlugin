3906f21dbde3f3222fda435dde6e02d2add87827
molsonkiko <46202915+molsonkiko@users.noreply.github.com>
2023-03-08 12:26:01 -0800

    add makerelease batch script

---
+++ makerelease.bat
@@ -0,0 +1,15 @@
+: this script requires 7-zip to be installed on your computer
+: sync testfiles with ones in repo
+xcopy .\testfiles .\JsonToolsNppPlugin\bin\Release-x64\testfiles\ /s /y
+copy ".\DSON UDL.xml" ".\JsonToolsNppPlugin\bin\Release-x64\DSON UDL.xml" /y
+xcopy .\testfiles .\JsonToolsNppPlugin\bin\Release\testfiles\ /s /y
+copy ".\DSON UDL.xml" ".\JsonToolsNppPlugin\bin\Release\DSON UDL.xml" /y
+: zip testfiles and dlls to release zipfiles
+: also copy directories to Downloads for easy access later
+cd JsonToolsNppPlugin\bin\Release-x64
+xcopy . "%userprofile%\Downloads\JsonTools NEWEST x64\" /s /y
+7z -r a ..\..\..\Release_x64.zip JsonTools.dll testfiles "DSON UDL.xml"
+cd ..\Release
+xcopy . "%userprofile%\Downloads\JsonTools NEWEST x86\" /s /y
+7z -r a ..\..\..\Release_x86.zip JsonTools.dll testfiles "DSON UDL.xml"
+cd ..\..\..
