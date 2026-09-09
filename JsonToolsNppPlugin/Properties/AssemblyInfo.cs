6835ad87690235a109d74d36adc54f0aee6e5a12
molsonkiko <molsonkiko@gmail.com>
2026-09-08 21:26:43 -0700

    force restore nuget packages in CI build Claude says that since my updates to the project in commit fa32a73e04a8cb3895a227630013ca25c5772938 (four months ago),     UnmanagedExports now lives in a Nuget package,     whereas previously it lived in a ".cs" file. Since Visual Studio restores all Nuget packages     whenever it compiles my project, but the CI pipeline didn't do that,     this change to CI_Build.yml should cause UnmanagedExports.Repack.Upgrade to be restored in the CI pipeline like it is on my computer. UnmanagedExports is required to make the DLL work as a Notepad++ plugin,     but it's not required to make a The change to the csproj is another change suggested by Claude,     to cause a compile error if UnmanagedExports.Repack.Upgrade is not found,     since that package is required to make the DLL work as a Notepad++ plugin,     but it's not required for the DLL to compile at all,     which is (presumably) why CI was succeeding even though the DLL produced was worthless.

--- JsonToolsNppPlugin/Properties/AssemblyInfo.cs
+++ JsonToolsNppPlugin/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("8.5.0.7")]
-[assembly: AssemblyFileVersion("8.5.0.7")]
+[assembly: AssemblyVersion("8.5.0.8")]
+[assembly: AssemblyFileVersion("8.5.0.8")]
